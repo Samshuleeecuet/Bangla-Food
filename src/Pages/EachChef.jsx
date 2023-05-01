6466e2cf9eba269e5ec4f0b560afa70659348a63
Samshul Islam Shakil <samshuleeeweb@gmail.com>
2023-05-02 01:55:58 +0600

    each chef data show

--- src/Pages/EachChef.jsx
+++ src/Pages/EachChef.jsx
@@ -5,7 +5,18 @@ const EachChef = ({chef}) => {
     //console.log(chef);
     const {id,chef_picture,chef_name,years_of_experience,likes,number_of_recipes}= chef;
     return (
-        <></>
+        <div className="card w-96 glass">
+        <figure><img src={chef_picture} alt="car!"/></figure>
+        <div className="card-body">
+            <h2 className="card-title">{chef_name}</h2>
+            <p>Experience: {years_of_experience}</p>
+            <p>Number of Recipes: {number_of_recipes}</p>
+            <p>Likes: {likes}</p>
+            <div className="card-actions justify-end">
+            <Link to={`/recepies/${id}`}><button className="btn btn-primary">View Recepies</button></Link>
+            </div>
+        </div>
+        </div>
     );
 };
 
