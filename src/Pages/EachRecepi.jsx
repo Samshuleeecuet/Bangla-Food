9608200fb12ec5363908c824f9ea67d0b9bebf0b
Samshul Islam Shakil <samshuleeeweb@gmail.com>
2023-05-02 11:55:55 +0600

    recepi and diasble work done

--- src/Pages/EachRecepi.jsx
+++ src/Pages/EachRecepi.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '@smastrom/react-rating/style.css';
+import { Rating } from '@smastrom/react-rating';
 
 const EachRecepi = ({recepi}) => {
     const {recipe_name,rating,cooking_method,ingredients}=recepi;
-    console.log(ingredients)
+    const [disable,setDisable] = useState(false)
+    const handleDisable =()=>{
+        setDisable(true);
+    }
+    //console.log(ingredients)
     return (
         <>
             <div className="card w-96 h-auto glass">
@@ -11,14 +17,23 @@ const EachRecepi = ({recepi}) => {
                     <p className='font-semibold'>Cooking Method:</p>
                     <p><small>{cooking_method}</small></p>
                     <p className='font-semibold'>Ingredients:</p>
-                    {
-                        ingredients.map((ingredient,index)=>{
-                            <p key={index}>{ingredient}</p>
-                        })
-                    }
-                    <div className="card-actions justify-end">
-                    <p>{rating}</p>
-                    <button className="btn btn-primary">Learn now!</button>
+                    <p>1. {ingredients[0]}</p>
+                    <p>2. {ingredients[1]}</p>
+                    <p>3. {ingredients[2]}</p>
+                    <p>4. {ingredients[3]}</p>
+                    <p>5. {ingredients[4]}</p>
+                    <div className="card-actions pt-5 flex">
+                    <div className='flex flex-grow'>
+                    <Rating
+                    style={{ maxWidth: 120 }}
+                    value={rating}
+                    readOnly
+                    />
+                    <span> {rating}</span>
+
+                    </div>
+                       
+                    <button onClick={handleDisable} disabled={disable} className="btn btn-primary">Favourite</button>
                     </div>
                 </div>
             </div>
